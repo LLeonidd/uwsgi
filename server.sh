00c705872ba121a591bd5c58dd31dd28ddaf21a4
Leonid <lzimin32@gmail.com>
2020-10-03 00:01:47 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
-#Запуск/Остановка/Перезагрузка uwsgi с доп. параметрами
+#Запуск/Остановка/Перезагрузка uwsgi с доп. параметрами, под разными пользователями
 
 name="app_name" #Название проекта
-cuser = uwsgi #Пользователь под которым запускать воркеры/процессы uwsgi
+cuser=uwsgi #Пользователь под которым запускать воркеры/процессы uwsgi
 ROOT="/path/to/project/$name" #Путь до проекта 
 pidfile=$ROOT/$name.pid 
 config=$name"_uwsgi.ini" # Название конфигурационного ini файла
@@ -9,7 +9,6 @@ cd $ROOT
 case "$1" in
     "start")
         sudo su - $cuser -c "uwsgi --ini $ROOT/$config" -s /bin/sh       
-        
         ;;
     "stop")
         sudo su - $cuser -c "kill -9 `cat $pidfile`" -s /bin/sh
